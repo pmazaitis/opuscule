8d806c216f8858d72da9a5633dbe51b8aebd3817
Paul Mazaitis <paul@mazaitis.org>
2021-06-15 22:24:18 -0400

    distribution of messages is working

--- opuscule-server/src/main.rs
+++ opuscule-server/src/main.rs
@@ -3,19 +3,31 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 const ADDR: &'static str = "127.0.0.1:8080";
+
+// set up a watch channel for commands up, and a broadcast channel for commands down?
+// how to send to a specific channel?
 
 #[tokio::main]
 async fn main() {
     // Set up a TcpListener object
     let listener = TcpListener::bind(&ADDR).await.unwrap();
 
+    // tokio broadcast channel; can we expose these?
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     //We need to put accept into a loop for catching new clients
     loop {
         // call accept to accept new incoming connection for a stream and addr
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        // We need to clone this so that each clent gets one
+        let tx = tx.clone();
+        // use this method to pull the receiver out of transmit (!?)
+        let mut rx = tx.subscribe();
 
         // shunt off each new client to a new task (what ahppens if we do this witha funciton?)
         let join_handle = tokio::spawn(async move {
@@ -29,15 +41,27 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                // pull in
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                // Test to see if stream has ended
-                if bytes_read == 0 {
-                    break;
-                }
-                // send out
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
+                // let's use select to decide what to do - return id =  future => block of code
+                // select awaits futures implicitly
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        // Test to see if stream has ended
+                        if result.unwrap() == 0 {break;}
+                        // send to all clients
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        // send out to this specific ui_client
+                        let msg = result.unwrap();
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                } // end tokio select
+                  // pull in
+                  //let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                // write out
+                // let msg = rx.recv().await.unwrap();
             } // end loop for echoing a specific client
         }); // end spawn
     } // end accept loop
