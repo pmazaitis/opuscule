825f26c867b7d088ec0bd48732a790dbe5cd19bb
Paul Mazaitis <paul@mazaitis.org>
2021-06-22 14:39:07 -0400

    working echo (no error handling yet)

--- opuscule-server/src/main.rs
+++ opuscule-server/src/main.rs
@@ -1,76 +1,39 @@
-// use std::io::BufRead;
-
-// use tokio::{
-//     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-//     net::TcpListener,
-//     sync::broadcast,
-// };
+use tokio::{sync::mpsc, sync::watch};
 
 mod ui_clients;
 
+// We eventually want to get this from config
 const ADDR: &'static str = "127.0.0.1:8080";
 
 #[tokio::main]
 async fn main() {
-    ui_clients::run_ui_client_server().await;
-    loop {}
+    // Set up channels here
+
+    //Channels
+    // We offer the ui_clients module the tx here, so we can get the commands it receives
+    let (ui_cmds_tx, mut ui_cmds_rx) = mpsc::channel::<String>(10);
+    // We offer the ui_clients module the rx so it can distribute state
+    let (state_tx, state_rx) = watch::channel::<String>(String::from("NOOP"));
+
+    let ui_client_server = ui_clients::handle_ui_clients(String::from(ADDR), ui_cmds_tx, state_rx);
+    tokio::spawn(ui_client_server);
+
+    loop {
+        // tokio::select! {
+        //     result = ui_cmds_rx.recv() => {
+        //         // Test to see if stream has ended
+        //         // if result.unwrap() == 0 {break;}
+        //         // send to all clients
+        //         // cmds_tx.send((line.clone(), addr)).unwrap();
+        //         println!("Got in server loop {:?}", result.unwrap());
+        //         let new_state = format!("Broadcasting result: {}", result.unwrap());
+
+        //         state_tx.send(new_state).unwrap();
+        //     }
+        // }
+        let result = ui_cmds_rx.recv().await.unwrap();
+        println!("Got in server loop {:?}", result);
+        state_tx.send(result).unwrap();
+        println!("State sent");
+    }
 }
-
-// #[tokio::main]
-// async fn main() {
-//     // Set up a TcpListener object
-//     let listener = TcpListener::bind(&ADDR).await.unwrap();
-
-//     // tokio broadcast channel; can we expose these?
-//     let (tx, _rx) = broadcast::channel(10);
-
-//     //We need to put accept into a loop for catching new clients
-//     loop {
-//         // call accept to accept new incoming connection for a stream and addr
-//         let (mut socket, addr) = listener.accept().await.unwrap();
-
-//         // We need to clone this so that each clent gets one
-//         let tx = tx.clone();
-//         // use this method to pull the receiver out of transmit (!?)
-//         let mut rx = tx.subscribe();
-
-//         // shunt off each new client to a new task (what ahppens if we do this witha funciton?)
-//         let _join_handle = tokio::spawn(async move {
-//             //handle read and write independantly
-//             let (reader, mut writer) = socket.split();
-
-//             // each client, respond to all messages
-//             //a place to put the data from the socket
-//             let mut reader = BufReader::new(reader);
-
-//             let mut line = String::new();
-
-//             loop {
-//                 // let's use select to decide what to do - return id =  future => block of code
-//                 // select awaits futures implicitly
-//                 tokio::select! {
-//                     result = reader.read_line(&mut line) => {
-//                         // Test to see if stream has ended
-//                         if result.unwrap() == 0 {break;}
-//                         // send to all clients
-//                         tx.send((line.clone(), addr)).unwrap();
-//                         line.clear();
-//                     }
-//                     result = rx.recv() => {
-//                         // send out to this specific ui_client
-//                         let (msg, other_addr) = result.unwrap();
-//                         println!("Sending from {:?}", other_addr);
-//                         if addr != other_addr {
-//                             writer.write_all(msg.as_bytes()).await.unwrap();
-//                         }
-//                     }
-//                 } // end tokio select
-//                   // pull in
-//                   //let bytes_read = reader.read_line(&mut line).await.unwrap();
-
-//                 // write out
-//                 // let msg = rx.recv().await.unwrap();
-//             } // end loop for echoing a specific client
-//         }); // end spawn
-//     } // end accept loop
-// }
--- opuscule-server/src/ui_clients/mod.rs
+++ opuscule-server/src/ui_clients/mod.rs
@@ -1,131 +1,73 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
+use std::{future::Future, str::FromStr};
+
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{unix::SocketAddr, TcpListener},
-    sync::broadcast::{self, Receiver, Sender},
+    sync::mpsc,
+    sync::watch,
 };
 
-const ADDR: &'static str = "127.0.0.1:8080";
+use mpsc::Sender;
+use watch::Receiver;
 
-// Ui Clients Module
-//
-// Set up a loop to capture clients that connect
-// expose an awaitable handle for the server logic to capture events
-// expose a handle for distributing new state
-#[derive(Clone, Debug)]
-struct UiMessages {
-    recvd_messages: Sender<(String, SocketAddr)>,
-    send_ui_state: Receiver<(String, SocketAddr)>,
-}
+// pub struct UiActor {
+//     commands: mpsc::Receiver<String>,
+//     state: watch::Sender<String>,
+// }
 
-pub async fn run_ui_client_server() {
+// impl UiActor {
+//     pub async fn get_command(&self) {
+//         &self.commands.recv()
+//     }
+//     pub async fn send_state(state: String) {}
+// }
+
+pub async fn handle_ui_clients(
+    addr: String,
+    send_command: Sender<String>,
+    recd_state: Receiver<String>,
+) -> ! {
     // Set up a TcpListener object
-    let listener = TcpListener::bind(&ADDR).await.unwrap();
+    let listener = TcpListener::bind(&addr).await.unwrap();
 
-    // TODO: reanme these to ui_rx, etc., and have another channel for server communication?
-
-    // tokio broadcast channel; can we expose these?
-    let (external_tx, _rx) = broadcast::channel(10);
-
-    let server_tx = external_tx.clone();
-    let mut server_rx = external_tx.subscribe();
-
-    // let ui_messages = UiMessages {
-    //     recvd_messages: server_tx,
-    //     send_ui_state: server_rx,
-    // };
-
-    //We need to put accept into a loop for catching new clients
+    // We need to loop to catch each new client as it comes in
     loop {
-        // call accept to accept new incoming connection for a stream and addr
-        let (mut socket, addr) = listener.accept().await.unwrap();
+        let (mut ui_socket, addr) = listener.accept().await.unwrap();
 
-        // We need to clone this so that each client gets one
-        let external_tx = external_tx.clone();
-        // use this method to pull the receiver out of transmit (!?)
-        let mut external_rx = external_tx.subscribe();
+        // clone the channels for commands and state
+        let send_command = send_command.clone();
+        let mut recd_state = recd_state.clone();
 
-        // shunt off each new client to a new task (what ahppens if we do this witha funciton?)
-        let _join_handle = tokio::spawn(async move {
+        let _ui_client_handle = tokio::spawn(async move {
             //handle read and write independantly
-            let (reader, mut writer) = socket.split();
+            let (ui_client_reader, mut ui_client_writer) = ui_socket.split();
 
-            // each client, respond to all messages
-            //a place to put the data from the socket
-            let mut reader = BufReader::new(reader);
-
+            let mut reader = BufReader::new(ui_client_reader);
             let mut line = String::new();
 
             loop {
-                // let's use select to decide what to do - return id =  future => block of code
-                // select awaits futures implicitly
                 tokio::select! {
                     result = reader.read_line(&mut line) => {
                         // Test to see if stream has ended
                         if result.unwrap() == 0 {break;}
                         // send to all clients
-                        external_tx.send((line.clone(), addr)).unwrap();
+                        send_command.send(line.clone()).await;
+                        println!("Got in client loop {}", line.clone());
+
                         line.clear();
                     }
-                    result = external_rx.recv() => {
-                        // send out to this specific ui_client
-                        let (msg, other_addr) = result.unwrap();
-                        println!("Sending from {:?}", other_addr);
-                        if addr != other_addr {
-                            writer.write_all(msg.as_bytes()).await.unwrap();
-                        }
+                    // result = recd_state.borrow() => {
+                    //    // ui_client_writer.write_all(result);
+                    // }
+                    result = recd_state.changed() => {
+                        println!("got state back for the clients:{}", *recd_state.borrow());
+                        //ui_client_writer.write_all(*recd_state.borrow());
                     }
-                } // end tokio select
-                  // pull in
-                  //let bytes_read = reader.read_line(&mut line).await.unwrap();
-
-                // write out
-                // let msg = rx.recv().await.unwrap();
-            } // end loop for echoing a specific client
-        }); // end spawn
-    } // end accept loop
+                }
+            }
+        });
+    }
 }
-
-// #[derive(Debug)]
-// #[allow(dead_code)]
-// struct MyActor {
-//     receiver: String,
-
-// }
-
-// #[derive(Debug)]
-// #[allow(dead_code)]
-// enum ClientCommandType {
-//     Play,
-//     Stop,
-//     Jump(u8),
-// }
-
-// #[derive(Debug)]
-// struct ClientCommand {
-//     addr: String,
-//     //command: ClientCommandType,
-//     command: String,
-// }
-
-// #![allow(unused)]
-
-// /// the ui_clients server instance.
-// pub struct UiClients {
-//     listener: TcpListener,
-//     connections: Arc<Mutex<VecMap<ClientConn>>>,
-//     ids: Arc<Mutex<VecMap<u8>>>, // If we have more than u8 clients, there's a problem
-//     new_r: Receiver<usize>,
-//     thread: JoinHandle<()>,
-// }
-
-// impl UiClients {
-//     pub fn new<A>(addr: A) -> io::Result<UiCLients>
-//     where
-//         A: ToSocketAddrs,
-//     {
-//         let listener = TcpListener::bind(&addr).await.unwrap();
-//         let connections = Arc::new(Mutex::new(VecMap::new()));
-//         let names = Arc::new(Mutex::new(VecMap::new()));
-//         let (new_s, new_r) = channel();
-//     }
-// }
