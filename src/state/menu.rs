eb856f596fb741a9469e5b014a74075ab5bb4661
Paul Mazaitis <paul@mazaitis.org>
2022-08-19 01:14:17 +0000

    menu reset II

--- src/state/menu.rs
+++ src/state/menu.rs
@@ -1,5 +1,4 @@
 
-use trees::Tree;
 
 // The menu module
 //
@@ -15,54 +14,3 @@ use trees::Tree;
 
 use crate::common::{Playable, OpResult, OpusID};
 
-enum OpMenuNode {
-    Opus(OpusID),
-    Label(MenuLabel),
-    Root,
-    // Command()
-}
-
-
-struct MenuLabel {
-    title: String,
-    short_title: Option<String>,
-    
-}
-
-
-struct Menu<'m>  {
-
-}
-
-impl Menu<'_> {
-    // Generate a new Menu object to keep track of:
-    //  - Entire menu tree of the appliance
-    //  - Current position in the menu tree
-    //
-    fn new() {
-
-    }
-    
-    // Add component tree 
-    fn add_component(tree: Tree) {
-        
-    }
-    
-    fn advance() {
-        
-    }
-    
-    fn retreat() {
-        
-    }
-    
-    fn select() {
-        
-    }
-    
-    fn escape() {
-        
-    }
-    
-    
-}
