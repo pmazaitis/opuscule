d5c11c7e25d4da64f917f1b93456c362f9a64fb2
Paul Mazaitis <paul@mazaitis.org>
2022-09-03 21:41:59 +0000

    removed state management now being handled int he state module

--- src/state/audio.rs
+++ src/state/audio.rs
@@ -1,8 +1,8 @@
 #[allow(unused_imports)] // FIXME remove this when we know what tracing options we need
 use tracing::{debug, error, info, trace, warn};
 
-use crate::common::{OpUICommand, OpUICommandType};
-use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
+// use crate::common::{OpUICommand, OpUICommandType};
+// use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
 
 trait Opus {}
 
@@ -89,79 +89,79 @@ impl Stopped {
     }
 }
 
-pub struct Controller {
-    state: AudioState,
-    audio_out: OutputStreamHandle,
-}
-
-impl Controller {
-    pub fn new() -> Controller {
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-
-        Controller {
-            state: AudioState::Stopped(Stopped {}),
-            audio_out: stream_handle,
-            // now playing, somehow?
-        }
-    }
-
-    // pub fn handle_command(&mut self, rc: OpUICommand) -> OpUICommand {
-    //     match rc.command {
-    //         OpUICommandType::Play => {
-    //             println!("Got Play");
-    //             self.state = self.state.on_play(Play);
-    //         }
-    //         OpUICommandType::Stop => {
-    //             println!("Got Stop");
-    //             self.state = self.state.on_stop(Stop);
-    //         }
-    //         OpUICommandType::Pause => {
-    //             println!("Got Pause");
-    //             self.state = self.state.on_pause(Pause);
-    //         }
-    //         OpUICommandType::Advance => {
-    //             println!("Got Advance");
-    //         }
-    //         OpUICommandType::Retreat => {
-    //             println!("Got Retreat");
-    //         }
-    //         OpUICommandType::Select => {
-    //             println!("Got Select");
-    //         }
-    //         OpUICommandType::Escape => {
-    //             println!("Got Escape");
-    //         }
-    //         OpUICommandType::Favorite { slot: favid } => {
-    //             println!("Got Fav: {}", favid);
-    //         }
-    //         OpUICommandType::Mute { set_to: muted } => match muted {
-    //             Some(m) => println!("Setting mute to {}", m),
-    //             None => println!("Toggling mute"),
-    //         },
-    //         OpUICommandType::Random { set_to: randomized } => match randomized {
-    //             Some(r) => println!("Setting random to {}", r),
-    //             None => println!("Toggling random"),
-    //         },
-    //         OpUICommandType::Repeat { set_to: repeating } => match repeating {
-    //             Some(r) => println!("Setting repeat to {}", r),
-    //             None => println!("Toggling repeat"),
-    //         },
-    //         OpUICommandType::Refresh => {
-    //             println!("Got Refresh request");
-    //         }
-    //         OpUICommandType::Next => {
-    //             println!("Got Next");
-    //         }
-    //         OpUICommandType::Previous => {
-    //             println!("Got Previous");
-    //         }
-    //         OpUICommandType::Louder => {
-    //             println!("Got Louder");
-    //         }
-    //         OpUICommandType::Softer => {
-    //             println!("Got Softer");
-    //         }
-    //     }
-    //     rc
-    // }
-}
+// pub struct Controller {
+//     state: AudioState,
+//     audio_out: OutputStreamHandle,
+// }
+// 
+// impl Controller {
+//     pub fn new() -> Controller {
+//         let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+// 
+//         Controller {
+//             state: AudioState::Stopped(Stopped {}),
+//             audio_out: stream_handle,
+//             // now playing, somehow?
+//         }
+//     }
+// 
+//     // pub fn handle_command(&mut self, rc: OpUICommand) -> OpUICommand {
+//     //     match rc.command {
+//     //         OpUICommandType::Play => {
+//     //             println!("Got Play");
+//     //             self.state = self.state.on_play(Play);
+//     //         }
+//     //         OpUICommandType::Stop => {
+//     //             println!("Got Stop");
+//     //             self.state = self.state.on_stop(Stop);
+//     //         }
+//     //         OpUICommandType::Pause => {
+//     //             println!("Got Pause");
+//     //             self.state = self.state.on_pause(Pause);
+//     //         }
+//     //         OpUICommandType::Advance => {
+//     //             println!("Got Advance");
+//     //         }
+//     //         OpUICommandType::Retreat => {
+//     //             println!("Got Retreat");
+//     //         }
+//     //         OpUICommandType::Select => {
+//     //             println!("Got Select");
+//     //         }
+//     //         OpUICommandType::Escape => {
+//     //             println!("Got Escape");
+//     //         }
+//     //         OpUICommandType::Favorite { slot: favid } => {
+//     //             println!("Got Fav: {}", favid);
+//     //         }
+//     //         OpUICommandType::Mute { set_to: muted } => match muted {
+//     //             Some(m) => println!("Setting mute to {}", m),
+//     //             None => println!("Toggling mute"),
+//     //         },
+//     //         OpUICommandType::Random { set_to: randomized } => match randomized {
+//     //             Some(r) => println!("Setting random to {}", r),
+//     //             None => println!("Toggling random"),
+//     //         },
+//     //         OpUICommandType::Repeat { set_to: repeating } => match repeating {
+//     //             Some(r) => println!("Setting repeat to {}", r),
+//     //             None => println!("Toggling repeat"),
+//     //         },
+//     //         OpUICommandType::Refresh => {
+//     //             println!("Got Refresh request");
+//     //         }
+//     //         OpUICommandType::Next => {
+//     //             println!("Got Next");
+//     //         }
+//     //         OpUICommandType::Previous => {
+//     //             println!("Got Previous");
+//     //         }
+//     //         OpUICommandType::Louder => {
+//     //             println!("Got Louder");
+//     //         }
+//     //         OpUICommandType::Softer => {
+//     //             println!("Got Softer");
+//     //         }
+//     //     }
+//     //     rc
+//     // }
+// }
